d5962e42785c4ffafc88e5fa1c8393b8a2859a90
Oded Shopen <odedia@gmail.com>
2020-01-20 18:15:03 +0200

    git ignore

---
+++ src/main/java/fr/alexandreroman/cloudnativepoll/webui/Application.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.alexandreroman.cloudnativepoll.webui;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@RestController
+@Slf4j
+class VotesController {
+    @GetMapping("/ping/{ip}/{port}")
+    String getPing(@PathVariable String ip, @PathVariable Integer port) {
+    	log.info("Got IP {} and port {}", ip, port);
+    	return pingHost(ip, port, 5000) ? "Yay, I could connect!" : "Bummer, I couldn't connect after 5 seconds.";    			
+    }
+    
+    public static boolean pingHost(String host, int port, int timeout) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), timeout);
+            return true;
+        } catch (IOException e) {
+            return false; 
+        }
+    }
+
+    
+}
